70e412932b128381f8089b01f43ce5de502fdbdb
Bruno M <declic62gmail.com>
2023-08-24 10:42:32 +0200

    feat: init and update admin.py

--- betterChanceSite/admin.py
+++ betterChanceSite/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from ..explanations.models import Author
-
-
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Author, AuthorAdmin)
--- betterChanceSite/settings.py
+++ betterChanceSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
@@ -126,5 +127,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
-    BASE_DIR / "betterChanceSite" / "static"
+     os.path.join(BASE_DIR, "betterChanceSite/static")
 ]
